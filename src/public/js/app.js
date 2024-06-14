b347a57b344f3d1b0687549e6137bfde99cbe027
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-06-14 15:59:39 +0900

    socket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,25 @@
-alert("gk..")
+// front
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// 방법1
+// socket.addEventListener("close", () => {
+//     console.log("app.js 연결해제")
+// })
+// 방법2
+function handleSocketClose (){
+    console.log("app.js 연결해제")
+}
+socket.addEventListener("close", handleSocketClose)
+
+socket.addEventListener("open", () => {
+    console.log("app.js 연결");
+})
+
+socket.addEventListener("message", (message) => {
+    console.log("뭐라고 보냈을까?", message.data)
+})
+
+setTimeout(() => {
+    socket.send("이것은 3초뒤에 보내지는 메세지.")
+}, 3000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
+// back
+
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express()
 
@@ -9,4 +13,26 @@ app.get("/", (req, res) => res.render("home"))
 app.get("/*", (req, res) => res.redirect("/"))
 
 const handleListen = () => console.log(`http://localhost:3000`)
-app.listen(3000, handleListen)
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+function handleSocketClose (){
+    console.log("server.js 연결해제")
+}
+
+wss.on("connection", (socket) => {
+    console.log("server.js 연결")
+    // 1 방법
+    // socket.on("close", () => console.log("server.js 연결해제"))
+    // 2 방법
+    socket.on("close", handleSocketClose)
+    socket.on("message", message => {
+        console.log(message.toString('utf8'));
+    });
+    socket.send("이것은 최초 메세지.")
+});
+
+server.listen(3000, handleListen);
+//

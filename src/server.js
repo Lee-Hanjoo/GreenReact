9af7c4ca36703482b4d8328eaa43afa45fdecd52
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-06-14 14:54:16 +0900

    front setup

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-const app = express();
+const app = express()
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views")
+app.use("/public", express.static(__dirname + "/public"))
+app.get("/", (req, res) => res.render("home"))
+app.get("/*", (req, res) => res.redirect("/"))
 
-const handleListen = () => console.log(`http://localhost:3000`);
+const handleListen = () => console.log(`http://localhost:3000`)
 app.listen(3000, handleListen)

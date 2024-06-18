a6ace5afc89cf0c0cd60507f701d09a39008bbaf
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-06-18 13:42:15 +0900

    push

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,14 @@
 // 프론트에서 socket을 연결
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRommSubmit(e) {
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("room");
+    input.value = ""
+}
+
+form.addEventListener("submit", handleRommSubmit);
